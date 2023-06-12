a1942304d26c3c067047ec67bbb1b112902e0b69
조흔나 <61869540+heunnajo@users.noreply.github.com>
2023-06-12 16:55:05 +0900

    Update DtiRepositoryTest.java

--- src/test/java/sbms/dti/DtiRepositoryTest.java
+++ src/test/java/sbms/dti/DtiRepositoryTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +20,7 @@ public class DtiRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(false)
     public void testDti() throws Exception{
         //given
         Dti dti = new Dti();
@@ -30,6 +32,7 @@ public class DtiRepositoryTest {
 
         //then
         Assertions.assertThat(findDti.getConversationID()).isEqualTo(dti.getConversationID());
+        //Assertions.assertThat((findDti).isEqualTo(dti));//같은 트랜잭션 내에서 저장, 조회 시에 같다고 나와야하고 그렇다!
     }
 //    @Test
 //    void contextLoads(){
